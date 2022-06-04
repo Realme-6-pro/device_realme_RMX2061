afc7676f4b41930a4fef94134fdd4baee956fff3
neel0210 <neeljadhav0210@gmail.com>
2022-06-04 13:44:02 +0530

    RMX2061: Initial rice droid

--- lineage_RMX2061.mk
+++ lineage_RMX2061.mk
@@ -12,16 +12,17 @@ TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GMS := true
 TARGET_GAPPS_ARCH := arm64
 USE_GAPPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Google Assistant
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
-
 # Inherit some common stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := aosp_RMX2061
+PRODUCT_NAME := lineage_RMX2061
 PRODUCT_DEVICE := RMX2061
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 6 Pro
@@ -40,3 +41,7 @@ BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX2061" \
     TARGET_DEVICE="RMX2061"
+
+# RIce
+RICE_MAINTAINER := Neel0210
+TARGET_HAS_UDFPS := true

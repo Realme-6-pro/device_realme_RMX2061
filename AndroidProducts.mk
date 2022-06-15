ed6fe4cb1a298838956e97617b6e44ce3f6087ca
neel0210 <neeljadhav0210@gmail.com>
2022-06-15 16:34:24 +0530

    RXM2061: Initial bringup Zephyrus

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX2061.mk
+    $(LOCAL_DIR)/RMX2061.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_RMX2061-user \
-    aosp_RMX2061-userdebug \
-    aosp_RMX2061-eng
+    RMX2061-user \
+    RMX2061-userdebug \
+    RMX2061-eng
--- RMX2061.mk
+++ RMX2061.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from RMX2061 device
 $(call inherit-product, device/realme/RMX2061/device.mk)
 
@@ -12,16 +16,14 @@ TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GMS := true
 TARGET_GAPPS_ARCH := arm64
 USE_GAPPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Google Assistant
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
-
-# Inherit some common stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
 # Device identifier
-PRODUCT_NAME := aosp_RMX2061
+PRODUCT_NAME := RMX2061
 PRODUCT_DEVICE := RMX2061
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 6 Pro
